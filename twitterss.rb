b9a82511f6448da35ccf6dc69eba6a16983ff379
mostro <mostro@WaterDragon.(none)>
2009-08-05 12:16:59 -0400

    Changes to title and URI

--- twitterss.rb
+++ twitterss.rb
@@ -16,15 +16,15 @@ require 'twitter'
 # http://www.igvita.com/2007/03/22/agile-rss-aggregator-in-ruby/
 
 class Twitterss
-  
+
   TWITTER_LIMIT = 140
-  
+
   @@past_posts_file = File.join(ENV['HOME'], '.twitterss_history')
-  
+
   def initialize(config_file, logger)
     @config = YAML.load(File.read(config_file))
     @logger = logger
-    
+
     File.open(@@past_posts_file, "w").close if !File.exists?(@@past_posts_file)
     @past_posts = YAML.load(File.read(@@past_posts_file))
     @past_posts ||= {}
@@ -33,33 +33,33 @@ class Twitterss
   def run
     @config.each do |name, config|
       @logger.debug "#{name}:"
-      @logger.debug "parsing #{config['rss']} " 
+      @logger.debug "parsing #{config['rss']} "
       max = config['max'] || 5
       feed = delete_already_posted(name,FeedNormalizer::FeedNormalizer.parse(open(config['rss'])) , max)
       feed.items[0,max].each do |item|
         generate_message(item)
-        
+
         if posted?(name, item)
           @logger.debug "\"#{item.title}\"...skipped"
         else
           twitter_client = Twitter::Client.new(:login => config['login'],:password => config['password'])
           twitter_client.status(:post, generate_message(item))
-          
+
           mark_posted(name, item)
           @logger.debug "\"#{item.title}\"...posted"
         end
       end
-      
+
     end
-  end  
-  
+  end
+
   def self.dump_usage
     puts "Usage:"
     puts "  twitterss [-v] <configuration file>"
   end
-  
+
   private
-  
+
   def delete_already_posted (name, feed, max)
     count = 0;
     while (max > count) && (feed.items.size>0)
@@ -80,41 +80,20 @@ class Twitterss
     @past_posts[name] ||= {}
     @past_posts[name][item.urls[0].href]
   end
-  
+
   def mark_posted(name, item)
     @past_posts[name] ||= {}
     @past_posts[name][item.urls[0].href] = true
     dump_state!
   end
-  
+
   def dump_state!
     @logger.debug "dumping state..."
     File.open(@@past_posts_file, "w") do |f|
       YAML.dump(@past_posts, f)
     end
   end
-  
-  # this two functions are a hack
-  # TODO:Fix this. Make the parsing of the links and titles mor ruby-ish
-  #      this looks like ANSI C !
-  def clean_link (link)        
-    if (link.include?('href='))
-      link = link.match(/href=["]?([^'"]*)[" ]/mi) 
-      link = link.to_s.strip
-      link = link[6,link.length - 7]
-    end
-    link        
-  end
-  
-  def clean_title (title)
-    if (title.include?('<title type'))
-      title = title.match(/[>]([^"]*)[<]/)
-      title = title.to_s.strip
-      title = title[1,title.length-2]
-    end
-    title 
-  end
-  
+
   def generate_message(item)
     link = item.urls[0].href.to_s
     puts
